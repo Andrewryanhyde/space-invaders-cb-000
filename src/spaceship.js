ae5e0a69cb449abe97311b42eb6a1639057101da
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-09-25 09:25:03 +0000

    automatically backed up by learn

--- src/spaceship.js
+++ src/spaceship.js
@@ -3,17 +3,17 @@ class Spaceship {
   constructor(name, crew, phasers, shields) {
     this.name = name;
     this.crew = crew;
-    this.phasers = phasers; 
-    this.shields = shields; 
-    this.cloaked = false; 
+    this.phasers = phasers;
+    this.shields = shields;
+    this.cloaked = false;
     this.phasersCharge = "uncharged";
     this.warpDrive = "disengaged";
     if (crew.length !== 0) {
       for (var i = 0; i < crew.length; i++) {
-        crew[i].currentShip = this; 
+        crew[i].currentShip = this;
       };
-      this.dicked = "true";
-    } 
-    
+      this.docked = false; 
+    }
+
   }
 }
